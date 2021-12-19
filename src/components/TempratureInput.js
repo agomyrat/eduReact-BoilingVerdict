45765d9925b7f0090606cdb2a59f6416ad80e500
Agamyrat <agamyrat.chariyev@gmail.com>
2021-12-19 17:07:40 +0500

    convertor

--- src/components/TempratureInput.js
+++ src/components/TempratureInput.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
-
+import styles from './tempratureInput.module.scss';
 class TempratureInput extends Component {
+    onChangeFunction(e) {
+        this.props.changeTemp(e.target.value);
+    }
     render() {
-        return <div></div>;
+        const temperature = this.props.temperature;
+        const labelInner = this.props.type === 'c' ? 'Celsius' : 'Fahrenheit';
+        const className = this.props.type === 'c' ? 'celsius' : 'fahrenheit';
+        return (
+            <div className={styles.formGroup}>
+                <label htmlFor={className}>{labelInner}</label>
+                <input
+                    type="text"
+                    name={className}
+                    id={className}
+                    value={temperature}
+                    onChange={this.onChangeFunction.bind(this)}
+                />
+            </div>
+        );
     }
 }
 
